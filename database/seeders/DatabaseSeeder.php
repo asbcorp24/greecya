48d4eb9b7b62c2f49a4a1714ddc12427001f40db
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-22 22:11:36 +0300

    feat: include business integration seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             PoolCrmSeeder::class,
             AccessControlSeeder::class,
             FamilySwimSeeder::class,
+            BusinessIntegrationSeeder::class,
         ]);
     }
 }
